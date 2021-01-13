1e276cd90f35c1e86854e410ab8e81012a28b116
Mason Phillips <mjphillips2000@gmail.com>
2021-01-13 21:29:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,9 +13,10 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
+      params[:pirate][:ships].each do |key|
+        Ship.new(key)
       end
+
       @ships = Ship.all
 
       erb :'pirates/show'
